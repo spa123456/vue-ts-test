28f5d8aefe2c79f2297bcb4bb5c456de7b77e067
shipingan <740425014@qq.com>
2019-11-05 13:41:34 +0800

    fix:安装了某些框架

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import axios from 'axios';
+import elementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
+Vue.use(elementUi)
 
 new Vue({
   router,
